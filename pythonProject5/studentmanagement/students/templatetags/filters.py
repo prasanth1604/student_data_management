05a8bdfaf283e5272de82ba1c7db5aca9e6c5acd
prasanth1604 <129258527+prasanth1604@users.noreply.github.com>
2024-06-02 01:41:25 +0530

    Delete pythonProject5/studentmanagement directory

--- pythonProject5/studentmanagement/students/templatetags/filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from students.models import Student ,Subject , Faculty
-
-register = template.Library()
-
-def searchName(value):
-    st = Student.objects.get(roll_no=value)
-    return st.name
